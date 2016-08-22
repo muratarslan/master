31199ff3862e649ace6c87c17c72b24906ea0f7e
Murat Arslan <wuratarslan@gmail.com>
2016-08-22 18:09:51 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+
+# Nao
+IP = "192.168.10.13"
+PORT = 9559
+CAMERAID = 18
+RESOLUTION =  2   # Image Size
+COLORSPACE = 11   # Select RGB
+
+
+# GUI
+GLADE_FILE_PATH = "ui/nao.glade"
+DATASET_DIRECTORY = "/home/murat/Desktop/Desktop/nao/dataset"
+NEGATIVE = "/home/murat/Desktop/Desktop/nao/dataset/negative"
+POSITIVE = "/home/murat/Desktop/Desktop/nao/dataset/positive"
+
+
---
+++ nao.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from gi.repository import Gtk
+import movement
+
+if __name__ == "__main__":
+        controller = movement.NaoController()
+	Gtk.main()
+
+# path finding icinde pathfinding
